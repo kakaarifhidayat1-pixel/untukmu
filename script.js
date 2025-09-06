66d9239ed78cbb23e20aebe75661fecc8ab0426e
kakaarifhidayat1-pixel <kakaarifhidayat1@gmail.com>
2025-09-06 18:25:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function gantiUcapan() {
+  document.getElementById("ucapan").innerHTML = `
+    Semoga hari-harimu selalu dipenuhi cahaya,<br>
+    setiap langkahmu dimudahkan,<br>
+    dan setiap doamu dijawab dengan indah. <br><br>
+    Semoga tahun ini jadi awal cerita baru<br>
+    yang penuh kebahagiaan dan keberkahan. ✨
+  `;
+}
